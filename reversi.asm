cb6e10bee2f7f4acfc264a3367889e951b4ccf12
batflyer <alexander@batflyer.net>
2017-11-27 23:21:01 -0600

    Initialize and Draw Board
    The DrawBoard function reads through the array of entries and outputs a human-readable board. 0 becomes a space, 1 becomes a X, and 2 becomes a O.

--- reversi.asm
+++ reversi.asm
@@ -2,7 +2,35 @@
 # Alexander L. Hayes
 # Computer Architecture 3340.001
 
+	#   12345678 
+	#  +--------+
+	# 1|        |
+	# 2|        |
+	# 3|        |
+	# 4|        |
+	# 5|        |
+	# 6|        |
+	# 7|        |
+	# 8|        |
+	#  +--------+
+
 .data
+	BoardPieceA: .asciiz "\n   12345678 \n  +--------+\n"
+	BoardPieceB: .asciiz " 1|"
+	BoardPieceC: .asciiz " 2|"
+	BoardPieceD: .asciiz " 3|"
+	BoardPieceE: .asciiz " 4|"
+	BoardPieceF: .asciiz " 5|"
+	BoardPieceG: .asciiz " 6|"
+	BoardPieceH: .asciiz " 7|"
+	BoardPieceI: .asciiz " 8|"
+	BoardPieceJ: .asciiz "  +--------+\n"
+	BoardRHS: .asciiz "|\n"
+	
+	X: .asciiz "X"
+	O: .asciiz "O"
+	SPACE: .asciiz " "
+	
 	EnterHorizontal:
 		.asciiz "\nEnter the horizontal number (1-8): "
 	InvalidFirstMessage:
@@ -16,13 +44,155 @@
 
 
 main:
-	# Begin the Program
-
-	jal UserChooseBoardPosition
+	# Begin the Program, Order of Operations:
+	#	0. Initialize an empty board.
+	#	1. Draw the Board.
+	#	2. User Chooses where to move. (update board)
+	#	3. Computer Chooses where to move. (update board)
+	#	4. Check for Game Over.
+	#	5. Jump to Step 1.
 	
+	# Initialize the Board before the main function begins.
+	jal InitializeBoard
 
 InitializeBoard:
 	# Initialize an empty board at the start of the program.
+
+	
+	# Each word in board is initialized as a 0.
+	# This is pretty much useless since everything starts out at zero.
+	
+	add $t0, $zero, $zero			# $t0 will be our pointer, start at 0 and loop to 256 (64 * 4)
+	
+	InitializeBoardLoop:
+
+	#addi $t1, $zero, 0
+	#sw $t1, Board($t0)
+	sw $zero, Board($t0)
+	addi $t0, $t0, 4			# Add 4 to the pointer ($t0) to move to the next word in memory.
+	
+	beq $t0, 256, DrawBoard			# Branch to DrawBoard when finished initializing the board.
+	jal InitializeBoardLoop
+	
+DrawBoard:
+	# Draw the board for Reversi, 0 becomes a space, 1 becomes an X, and 2 becomes an O
+	# Redo most of this with a macro if you find the time. Use two A registers to keep track of position and increment by 32.
+	li $v0, 4				# Load 4 into $v0, print_string opcode.
+	la $a0, BoardPieceA
+	syscall
+	
+	la $a0, BoardPieceB
+	syscall
+	add $t0, $zero, $zero
+	FirstDrawBoardLoop:
+	lw $a1, Board($t0)
+	addi $t0, $t0, 4
+	jal DrawSymbol
+	bne $t0, 32, FirstDrawBoardLoop
+	la $a0, BoardRHS
+	syscall
+	
+	la $a0, BoardPieceC
+	syscall
+	addi $t0, $zero, 32
+	SecondDrawBoardLoop:
+	lw $a1, Board($t0)
+	addi $t0, $t0, 4
+	jal DrawSymbol
+	bne $t0, 64, SecondDrawBoardLoop
+	la $a0, BoardRHS
+	syscall
+	
+	la $a0, BoardPieceD
+	syscall
+	addi $t0, $zero, 64
+	ThirdDrawBoardLoop:
+	lw $a1, Board($t0)
+	addi $t0, $t0, 4
+	jal DrawSymbol
+	bne $t0, 96, ThirdDrawBoardLoop
+	la $a0, BoardRHS
+	syscall
+	
+	la $a0, BoardPieceE
+	syscall
+	addi $t0, $zero, 96
+	FourthDrawBoardLoop:
+	lw $a1, Board($t0)
+	addi $t0, $t0, 4
+	jal DrawSymbol
+	bne $t0, 128, FourthDrawBoardLoop
+	la $a0, BoardRHS
+	syscall
+	
+	la $a0, BoardPieceF
+	syscall
+	addi $t0, $zero, 128
+	FifthDrawBoardLoop:
+	lw $a1, Board($t0)
+	addi $t0, $t0, 4
+	jal DrawSymbol
+	bne $t0, 160, FifthDrawBoardLoop
+	la $a0, BoardRHS
+	syscall
+	
+	la $a0, BoardPieceG
+	syscall
+	addi $t0, $zero, 160
+	SixthDrawBoardLoop:
+	lw $a1, Board($t0)
+	addi $t0, $t0, 4
+	jal DrawSymbol
+	bne $t0, 192, SixthDrawBoardLoop
+	la $a0, BoardRHS
+	syscall
+
+	la $a0, BoardPieceH
+	syscall
+	addi $t0, $zero, 192
+	SeventhDrawBoardLoop:
+	lw $a1, Board($t0)
+	addi $t0, $t0, 4
+	jal DrawSymbol
+	bne $t0, 224, SeventhDrawBoardLoop
+	la $a0, BoardRHS
+	syscall
+	
+	la $a0, BoardPieceI
+	syscall
+	addi $t0, $zero, 224
+	EighthDrawBoardLoop:
+	lw $a1, Board($t0)
+	addi $t0, $t0, 4
+	jal DrawSymbol
+	bne $t0, 256, EighthDrawBoardLoop
+	la $a0, BoardRHS
+	syscall
+	
+	la $a0, BoardPieceJ
+	syscall
+	
+	j UserChooseBoardPosition
+
+DrawSymbol:
+	# Helper function to draw a certain function depending on what the argument contains.
+	beq $a1, $zero, DrawSpace
+	beq $a1, 1, DrawX
+	beq $a1, 2, DrawO
+	jr $ra
+
+	DrawX:
+		la $a0, X
+		syscall
+		jr $ra
+	DrawO:
+		la $a0, O
+		syscall
+		jr $ra
+	DrawSpace:
+		la $a0, SPACE
+		syscall
+		jr $ra
 
 UserChooseBoardPosition:
 	# Ask the user for the board position they wish to place a piece on.
@@ -66,7 +236,7 @@ UserChooseBoardPosition:
 	# /test
 	syscall
 	
-	jal main
+	jal DrawBoard
 	
 InvalidChoice:
 	# The user entered an invalid Board Position, have them try again.
